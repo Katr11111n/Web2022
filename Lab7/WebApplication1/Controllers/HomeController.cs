5ef86a7323f91646e52f8ad5ba2f5fc57b5aa089
Kasia <katyashkapogidaeva@gmail.com>
2022-05-29 05:09:51 +0300

    Lab 7

---
+++ Lab7/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            int firstVal = 10;
+            int secondVal = 5;
+            int result = firstVal / secondVal;
+
+            ViewBag.Message = "Отладка приложения ASP.NET MVC!";
+
+            return View(result);
+        }
+    }
+}
